90cacec6d7b9166ccef8ff92bffc8fe1a98ad38b
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-14 23:00:23 -0500

    feat(Funciones): :sparkles: declaracion

---
+++ main.js
@@ -0,0 +1,9 @@
+//Funciones por declaración
+//Esta forma permite declarar una función que existirá a lo largo de todo el código:
+
+function saludar() {
+    return "Hola";
+  }
+  
+  saludar(); // 'Hola'
+  typeof saludar; // 'function'
